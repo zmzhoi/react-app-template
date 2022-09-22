dba97c437ecb903ebdb116e2b16f0128492963fb
zmzhoi <zmzhoi@gmail.com>
2022-09-22 23:22:48 +0900

    add: css production mode 환경 설정
    * mini-css-extract-plugin, css-minimizer-webpack-plugin 추가
    * production 인 경우, css 파일 번들링 추출 및 압축 진행.

--- scripts/configs/webpack.config.js
+++ scripts/configs/webpack.config.js
@@ -2,15 +2,19 @@ const path = require('path');
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ESLintPlugin = require('eslint-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
 const { fixPublicUrl } = require('../utils');
 const paths = require('./paths');
+
+const moduleID = Symbol();
 
 module.exports = function (env) {
   const extensions = ['.js', '.jsx', '.ts', '.tsx'];
   const isProduction = env === 'production';
 
-  return {
+  const webpackConfig = {
     mode: env,
     entry: {
       app: paths.entry, // name: 'app'
@@ -18,7 +22,7 @@ module.exports = function (env) {
     output: {
       path: paths.output,
       publicPath: fixPublicUrl(process.env.PUBLIC_URL),
-      filename: isProduction ? 'js/[name].[contenthash:8].js' : 'js/bundle.js', // = output.path + output.filename
+      filename: 'js/bundle.js', // = output.path + output.filename
     },
     resolve: {
       extensions,
@@ -28,20 +32,18 @@ module.exports = function (env) {
       rules: [
         // Apply babel-loader
         {
+          [moduleID]: 'BabelHandling',
           test: /\.(js|jsx|ts|tsx)$/,
           include: paths.src,
           loader: 'babel-loader',
         },
         {
           // Apply style-loader + css-loader
+          [moduleID]: 'CssHandling',
           test: /\.css$/,
           use: ['style-loader', 'css-loader'],
         },
       ],
-    },
-    optimization: {
-      minimize: isProduction,
-      // minimizer ...
     },
     plugins: [
       new HtmlWebpackPlugin({
@@ -56,4 +58,29 @@ module.exports = function (env) {
       }),
     ],
   };
+
+  if (isProduction) {
+    // Set Optimization.
+    webpackConfig.optimization = {
+      minimize: true,
+      minimizer: [],
+    };
+
+    // Change name of bundle file.
+    webpackConfig.output.filename = 'js/[name].[contenthash:8].js';
+
+    // Change handling css.
+    const cssRule = webpackConfig.module.rules.find((rule) => rule[moduleID] === 'CssHandling');
+    cssRule.use = [MiniCssExtractPlugin.loader, 'css-loader'];
+    webpackConfig.plugins.push(
+      new MiniCssExtractPlugin({
+        filename: 'css/[name].[contenthash:8].css',
+      }),
+    );
+    webpackConfig.optimization.minimizer.push(new CssMinimizerPlugin());
+
+    // others..
+  }
+
+  return webpackConfig;
 };
