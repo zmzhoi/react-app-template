398eb4adad6b771d8f5220bc2da43b080b206058
zmzhoi <zmzhoi@gmail.com>
2022-10-09 00:16:55 +0900

    add: env.d.ts 에 환경 변수 추가

--- src/types/env.d.ts
+++ src/types/env.d.ts
@@ -2,5 +2,7 @@ declare namespace NodeJS {
   interface ProcessEnv {
     readonly NODE_ENV: 'development' | 'production' | 'test';
     readonly PUBLIC_URL: string;
+    readonly WEBPACK_ENV: 'development' | 'production';
+    readonly DEPLOY: 'true' | 'false';
   }
 }
